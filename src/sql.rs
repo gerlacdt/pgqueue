480f6c56eefd8c88d231291e7a03c147782b26df
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2023-09-17 14:57:48 +0200

    clean up

--- src/sql.rs
+++ src/sql.rs
@@ -44,29 +44,6 @@ impl Messenger {
         Messenger { pool }
     }
 
-    pub async fn find_by_id(&self, id: i32) -> Result<MessageEntity, sqlx::Error> {
-        let record = sqlx::query!(
-            r#"
-            SELECT id, status AS "status!: MessageStatus", payload, created_at, updated_at
-            FROM messages
-            WHERE id = $1;
-"#,
-            id
-        )
-        .fetch_one(&self.pool)
-        .await?;
-
-        let entity = MessageEntity {
-            id: record.id,
-            status: record.status,
-            payload: record.payload.into(),
-            created_at: record.created_at,
-            updated_at: record.updated_at,
-        };
-
-        Ok(entity)
-    }
-
     pub async fn add(&self, message: Message) -> Result<MessageEntity, sqlx::Error> {
         let record = sqlx::query!(
             "INSERT INTO messages(status, payload) VALUES ($1, $2)
@@ -163,7 +140,7 @@ mod tests {
     async fn process_next_test() {
         let pool = get_pool().await;
         setup_db(pool.clone()).await.unwrap();
-        let sut = Messenger::new(pool);
+        let sut = Messenger::new(pool.clone());
         let payload = Payload {
             version: 1,
             kind: "Command".to_owned(),
@@ -179,7 +156,7 @@ mod tests {
         };
         sut.process_next(&process_fn).await.unwrap();
 
-        let actual = sut.find_by_id(saved_message.id).await.unwrap();
+        let actual = find_by_id(pool.clone(), saved_message.id).await.unwrap();
 
         assert_eq!(MessageStatus::Completed, actual.status);
         assert_eq!(saved_message.id, actual.id);
@@ -196,38 +173,26 @@ mod tests {
         Ok(())
     }
 
-    // #[tokio::test]
-    // async fn listen_test() {
-    //     let pool = get_pool().await;
-    //     let pool2 = pool.clone();
-    //     setup_db(pool.clone()).await.unwrap();
+    async fn find_by_id(pool: PgPool, id: i32) -> Result<MessageEntity, sqlx::Error> {
+        let record = sqlx::query!(
+            r#"
+            SELECT id, status AS "status!: MessageStatus", payload, created_at, updated_at
+            FROM messages
+            WHERE id = $1;
+"#,
+            id
+        )
+        .fetch_one(&pool)
+        .await?;
 
-    //     tokio::spawn(async move {
-    //         println!("started tokio spawn()");
-    //         let process_fn = |entity: &MessageEntity| {
-    //             println!("processFn(): message.id: {:?}", entity.id);
-    //             println!("processFn(): message.payload: {:?}", entity.payload);
-    //         };
-    //         match listen(pool, process_fn).await {
-    //             Err(err) => println!("ERROR in listener, {:?}", err),
-    //             _ => println!("listner should bloc"),
-    //         }
-    //     });
+        let entity = MessageEntity {
+            id: record.id,
+            status: record.status,
+            payload: record.payload.into(),
+            created_at: record.created_at,
+            updated_at: record.updated_at,
+        };
 
-    //     // NOTIFY channel, with add() new message into channel
-    //     let payload = Payload {
-    //         version: 1,
-    //         kind: "Command".to_owned(),
-    //         message: "my new message notification".to_owned(),
-    //     };
-    //     let msg = Message {
-    //         payload: json!(payload),
-    //     };
-    //     let sut = Messenger::new(pool2);
-    //     sut.add(msg).await.unwrap();
-
-    //     sleep(Duration::from_millis(5000));
-
-    //     assert!(false);
-    // }
+        Ok(entity)
+    }
 }
